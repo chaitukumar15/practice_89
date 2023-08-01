6a9a0b2754c0b1f5c17e038ba7047565fe05f402
chaitukumar15 <108114098+chaitukumar15@users.noreply.github.com>
2023-08-01 07:56:06 +0530

    practice git

---
+++ basic.js
@@ -0,0 +1,40 @@
+
+
+[
+    {
+        "hello":"chaitanya",
+        "name":"kumar",
+        "sub":"bench"
+    },{
+        "hello":"chaitanya1",
+        "name":"kumar1",
+        "sub":"bench1"
+    },
+        {
+            "hello":"chaitanya1",
+            "name":"kumar1",
+            "sub":"bench1"
+        }
+    
+]
+
+
+
+
+
+
+
+
+
+
+
+
+
+// console.log(num.startsWith("0"));
+// let arr=["8","9","7","6"]
+// if(){
+
+// }else{
+
+
+// }
